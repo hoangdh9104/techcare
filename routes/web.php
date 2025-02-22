bab7db59b7ea5c4a19da52bc2cc011c08d7cb1c8
hoangdh9104 <150943690+hoangdh9104@users.noreply.github.com>
2025-02-22 22:43:49 +0700

    Revert "Phuong"

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\Frontend\HomeController;
-use App\Http\Controllers\Frontend\UserDashboardController;
-=======
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ShipperController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
->>>>>>> f9bdee78158177ce15b326b346844b39582a8af6
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,16 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/doshboard', function(){
-   
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::group(['middleware' => ['auth', 'verified'], 'prefix'=>'user', 'as'=>'user.'], function(){
-    Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
-})
-=======
 Route::get('/', function () {
     return view('welcome');
 });
@@ -47,4 +32,3 @@ Route::middleware('auth')->group(function () {
 });
 Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
 require __DIR__ . '/auth.php';
->>>>>>> f9bdee78158177ce15b326b346844b39582a8af6
