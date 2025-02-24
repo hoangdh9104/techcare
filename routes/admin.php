e3396957f1181e16a8ec88575c4185521c3d9af8
Nguyễn Hải Anh <125695015+Haianhyoyo@users.noreply.github.com>
2025-02-24 17:23:40 +0700

    admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -2,11 +2,13 @@
 // Admin routes
 
 use App\Http\Controllers\Backend\AdminController;
+
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 // admin routes
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
 // Profile routes
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
@@ -18,4 +20,7 @@ Route::put('change-status',[CategoryController::class,'changeStatus'])->name('ch
 Route::resource('category', CategoryController::class);
 
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+
+/** Category Route */
 
