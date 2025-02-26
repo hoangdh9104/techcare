788ba84976479433a609e55b2df5dbfae5423cb1
THUONGQUANG <nguyenquangthuong23082004@gmail.com>
2025-02-26 21:56:48 +0700

    migrate products

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -45,6 +45,7 @@
             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Product</span></a>
             <ul class="dropdown-menu">
               <li class="{{setActive(['admin.brand.*'])}}"><a class="nav-link" href="{{route('admin.brand.index')}}">Brand</a></li>
+              <li class="{{setActive(['admin.brand.*'])}}"><a class="nav-link" href="{{route('admin.brand.index')}}">Product</a></li>
             </ul>
         </li>
         {{-- end Manage Product --}}
