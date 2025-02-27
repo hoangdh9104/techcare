e9da7aeccd79447b21e59a58b6e2e4c6ad532c36
THUONGQUANG <nguyenquangthuong23082004@gmail.com>
2025-02-27 10:38:19 +0700

    product features create product and show datatable product

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -31,6 +31,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.bootstrap5.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"/>
     <link rel="stylesheet" href="{{asset('backend/assets/css/bootstrap-iconpicker.min.css')}}">
+    {{-- datetimepicker --}}
     <link rel="stylesheet" href="{{asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.css')}}">
     <link rel="stylesheet" href="{{asset('backend/assets/modules/select2/dist/css/select2.min.css')}}">
     <!-- Template CSS -->
@@ -136,6 +137,7 @@
     <script src="https://cdn.datatables.net/2.2.2/js/dataTables.bootstrap5.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{asset('backend/assets/js/bootstrap-iconpicker.bundle.min.js')}}"></script>
+    {{-- datetimepicker js --}}
     <script src="{{asset('backend/assets/modules/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
     <script src="{{asset('backend/assets/modules/select2/dist/js/select2.full.min.js')}}"></script>
     <!-- Page Specific JS File -->
