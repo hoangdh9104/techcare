9889bad1e9f06baf81e5ecd3e07180e1179adda8
Tungptpp02972 <Tungptpp02972@fpt.edu.com>
2025-03-24 16:40:07 +0700

    fig order active

--- resources/views/vendor/layouts/sidebar.blade.php
+++ resources/views/vendor/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
             alt="logo" class="img-fluid"></a>
     <ul class="dashboard_link">
         <li><a class="active" href="{{ route('vendor.dashboard') }}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
-        <li><a class="active" href="{{ route('vendor.orders') }}"><i class="fas fa-tachometer"></i>Orders</a></li>
+        <li><a  href="{{ route('vendor.orders') }}"><i class="fas fa-list-ul"></i>Orders</a></li>
         <li><a href="{{ route('vendor.products.index') }}"><i class="far fa-user"></i>Products</a></li>
         <li><a href="{{ route('vendor.shop-profile.index') }}"><i class="far fa-user"></i>Shop Profile</a></li>
         <li><a href="{{ route('vendor.profile') }}"><i class="far fa-user"></i> My Profile</a></li>
