0fdaae2d351da593cb7cd4b19a9bb1ae8143f3b2
ducknamya2107 <example@email.com>
2025-03-29 19:33:16 +0700

    lỗi conflic

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -59,11 +59,14 @@
                             href="{{ route('admin.seller-products.index') }}">Seller Product</a></li>
                     <li class="{{ setActive(['admin.seller-pending-products.*']) }}"><a class="nav-link"
                             href="{{ route('admin.seller-pending-products.index') }}">Seller Pending Product</a></li>
+<<<<<<< HEAD
+=======
 
                     <li class="{{ setActive(['admin.reviews.*']) }}"><a class="nav-link"
                             href="{{ route('admin.reviews.index') }}">Product Reviews</a></li>
 
 
+>>>>>>> new-branch-Nam2
                 </ul>
             </li>
 
@@ -99,6 +102,8 @@
                     <i class="far fa-square"></i>
                     <span>Advertisement</span>
                 </a>
+<<<<<<< HEAD
+=======
             </li>
 
             <li
@@ -117,6 +122,7 @@
                             href="{{ route('admin.vendor-requests.index') }}">Pending Vendors</a></li>
 
                 </ul>
+>>>>>>> new-branch-Nam2
             </li>
             <li><a class="nav-link" href="{{ route('admin.settings.index') }}"><i class="far fa-square"></i>
                     <span>Settings</span></a></li>
