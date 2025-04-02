020aa49fb00e593550c5ceb737f164ff29911317
THUONGQUANG <nguyenquangthuong23082004@gmail.com>
2025-04-02 07:55:14 +0700

    s

--- app/Http/Controllers/Frontend/UserOrderController.php
+++ app/Http/Controllers/Frontend/UserOrderController.php
@@ -34,7 +34,11 @@ class UserOrderController extends Controller
         if (!in_array($order->order_status, ['pending', 'processed_and_ready_to_ship'])) {
             return response()->json(['status' => 'error', 'message' => 'This order cannot be cancelled'], 400);
         }
-
+        // Kiểm tra nếu đơn hàng đã thanh toán qua thẻ ví (payment_status = 1)
+        if ($order->payment_status === 1) {
+            $order->payment_status = 0; // Đánh dấu thanh toán chưa thực hiện (vì đang hủy đơn)
+            $order->save();
+        }
         // Cập nhật trạng thái đơn hàng
         $order->update([
             'order_status' => 'canceled',
@@ -46,7 +50,7 @@ class UserOrderController extends Controller
             'order_id' => $order->id,
             'status' => 'canceled',
             'updated_by' => auth()->id(),
-            'reason' => 'User canceled order. Reason: ' . $request->reason
+            'reason' => 'Reason: ' . $request->reason
         ]);
 
         return response()->json(['status' => 'success', 'message' => 'Order has been canceled and saved to history successfully!']);
