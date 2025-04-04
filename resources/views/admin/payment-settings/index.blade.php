de9aa3a3f6422d0ab2496596abb4e828795680fe
ducknamya2107 <example@email.com>
2025-04-05 04:14:58 +0700

    comiit

--- resources/views/admin/payment-settings/index.blade.php
+++ resources/views/admin/payment-settings/index.blade.php
@@ -14,7 +14,7 @@
       <div class="row">
         <div class="col-12">
             <div class="card">
-               
+
                 <div class="card-body">
                   <div class="row">
                     <div class="col-2">
@@ -35,7 +35,7 @@
                           In quis non esse eiusmod sunt fugiat magna pariatur officia anim ex officia nostrud amet nisi pariatur eu est id ut exercitation ex ad reprehenderit dolore nostrud sit ut culpa consequat magna ad labore proident ad qui et tempor exercitation in aute veniam et velit dolore irure qui ex magna ex culpa enim anim ea mollit consequat ullamco exercitation in.
                         </div>
                         <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
-                          Lorem ipsum culpa in ad velit dolore anim labore incididunt do aliqua sit veniam commodo elit dolore do labore occaecat laborum sed quis proident fugiat sunt pariatur. Cupidatat ut fugiat anim ut dolore excepteur ut voluptate dolore excepteur mollit commodo. 
+                          Lorem ipsum culpa in ad velit dolore anim labore incididunt do aliqua sit veniam commodo elit dolore do labore occaecat laborum sed quis proident fugiat sunt pariatur. Cupidatat ut fugiat anim ut dolore excepteur ut voluptate dolore excepteur mollit commodo.
                         </div>
                       </div>
                     </div>
@@ -47,4 +47,4 @@
     </div>
 </section>
 @endsection
-h
+
