c4c401e637c6b2f6e26e3b04706b8d92d30bcff4
ducknamya2107 <example@email.com>
2025-04-05 00:37:56 +0700

    Withdraw vendor nhưng chưa hoàn thiện

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class OrderProduct extends Model
 {
     use HasFactory;
+    protected $table = 'order_products'; // Đảm bảo tên bảng đúng
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id', 'id');
+    }
 }
--- resources/views/vendor/layouts/sidebar.blade.php
+++ resources/views/vendor/layouts/sidebar.blade.php
@@ -14,7 +14,7 @@
                     class="far fa-cart-plus"></i>Products</a></li>
         <li><a class="{{ setActive(['vendor.reviews.index']) }}" href="{{ route('vendor.reviews.index') }}"><i
                     class="far fa-star"></i>Review</a></li>
-        <li><a class="{{ setActive(['vendor.reviews.index']) }}" href="{{ route('vendor.reviews.index') }}"><i
+        <li><a class="{{ setActive(['vendor.reviews.index']) }}" href="{{ route('vendor.withdraw.index') }}"><i
                     class="far fa-star"></i>My Withdraw</a></li>
         <li><a class="{{ setActive(['vendor.shop-profile.index']) }}"
                 href="{{ route('vendor.shop-profile.index') }}"><i class="far fa-user"></i>Shop Profile</a></li>
