056ecb16b78d2de46918ab02c4610d6a5c819bbc
ducknamya2107 <example@email.com>
2025-04-07 05:59:31 +0700

    Save progress before reset

--- app/Http/Controllers/Backend/PaymentSettingController.php
+++ app/Http/Controllers/Backend/PaymentSettingController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\CodSetting;
+use App\Models\MomoSetting;
 use App\Models\PaypalSetting;
+use App\Models\StripeSetting;
 use Illuminate\Http\Request;
 
 class PaymentSettingController extends Controller
@@ -11,6 +14,9 @@ class PaymentSettingController extends Controller
     public function index()
     {
         $paypalSetting = PaypalSetting::first();
-        return view('admin.payment-settings.index', compact('paypalSetting'));
+        $stripeSetting = StripeSetting::first();
+        $momoSetting = MomoSetting::first();
+        $codSetting = CodSetting::first();
+        return view('admin.payment-settings.index', compact('paypalSetting', 'stripeSetting','momoSetting','codSetting'));
     }
 }
