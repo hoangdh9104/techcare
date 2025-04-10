1ed8223b91866735cb6180a6ec218c997528e1ba
hoangdh9104 <hoangkind9104@gmail.com>
2025-04-10 12:45:38 +0700

    feat(chat): update profile receiver in admin

--- resources/views/admin/messenger/index.blade.php
+++ resources/views/admin/messenger/index.blade.php
@@ -19,44 +19,19 @@
                         </div>
                         <div class="card-body">
                             <ul class="list-unstyled list-unstyled-border">
-                                <li class="media">
-                                    <img alt="image" class="mr-3 rounded-circle" width="50"
-                                        src="assets/img/avatar/avatar-1.png">
-                                    <div class="media-body">
-                                        <div class="mt-0 mb-1 font-weight-bold">Hasan Basri</div>
-                                        <div class="text-success text-small font-600-bold"><i class="fas fa-circle"></i>
-                                            Online</div>
-                                    </div>
-                                </li>
-                                <li class="media">
-                                    <img alt="image" class="mr-3 rounded-circle" width="50"
-                                        src="assets/img/avatar/avatar-2.png">
-                                    <div class="media-body">
-                                        <div class="mt-0 mb-1 font-weight-bold">Bagus Dwi Cahya</div>
-                                        <div class="text-small font-weight-600 text-muted"><i class="fas fa-circle"></i>
-                                            Offline</div>
-                                    </div>
-                                </li>
-                                <li class="media">
-                                    <img alt="image" class="mr-3 rounded-circle" width="50"
-                                        src="assets/img/avatar/avatar-3.png">
-                                    <div class="media-body">
-                                        <div class="mt-0 mb-1 font-weight-bold">Wildan Ahdian</div>
-                                        <div class="text-small font-weight-600 text-success"><i class="fas fa-circle"></i>
-                                            Online
+                                @foreach ($chatUsers as $chatUser)
+                                    <li class="media">
+                                        <img alt="image" class="mr-3 rounded-circle" width="50"
+                                            src="{{ asset($chatUser->senderProfile->image) }}">
+                                        <div class="media-body">
+                                            <div class="mt-0 mb-1 font-weight-bold">{{ $chatUser->senderProfile->name }}
+                                            </div>
+                                            {{-- <div class="text-success text-small font-600-bold"><i class="fas fa-circle"></i>
+                                                Online</div> --}}
                                         </div>
-                                    </div>
-                                </li>
-                                <li class="media">
-                                    <img alt="image" class="mr-3 rounded-circle" width="50"
-                                        src="assets/img/avatar/avatar-4.png">
-                                    <div class="media-body">
-                                        <div class="mt-0 mb-1 font-weight-bold">Rizal Fakhri</div>
-                                        <div class="text-small font-weight-600 text-success"><i class="fas fa-circle"></i>
-                                            Online
-                                        </div>
-                                    </div>
-                                </li>
+                                    </li>
+                                @endforeach
+
                             </ul>
                         </div>
                     </div>
