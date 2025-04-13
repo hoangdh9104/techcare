ae901d195c573330289391215101cf99b988d5ce
ducknamya2107 <example@email.com>
2025-04-13 12:36:03 +0700

    cập nhật tiếng việt manager-useruser

--- resources/views/admin/manage-user/index.blade.php
+++ resources/views/admin/manage-user/index.blade.php
@@ -4,7 +4,7 @@
       <!-- Main Content -->
         <section class="section">
           <div class="section-header">
-            <h1>Manage User</h1>
+            <h1>Quản lí người dùng</h1>
           </div>
 
           <div class="section-body">
@@ -13,16 +13,16 @@
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>Manage User</h4>
+                    <h4>Quản lí người dùng</h4>
 
                   </div>
                   <div class="card-body">
 
                     <form action="{{route('admin.manage-user.create')}}" method="POST">
                         @csrf
-                        
+
                         <div class="form-group">
-                            <label>Name</label>
+                            <label>Tên</label>
                             <input type="text" class="form-control" name="name" value="">
                         </div>
                         <div class="form-group">
@@ -32,27 +32,27 @@
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label>Password</label>
+                                    <label>Mật khẩu</label>
                                     <input type="password" class="form-control" name="password" value="">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label>Confirm Password</label>
+                                    <label>Xác nhận mật khẩu</label>
                                     <input type="password" class="form-control" name="password_confirmation" value="">
                                 </div>
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="inputState">Status</label>
+                            <label for="inputState">Trạng thái</label>
                             <select id="inputState" class="form-control" name="role">
-                                <option value="">Select</option>
+                                <option value="">Lựa chọn</option>
                               <option value="user">User</option>
-                              <option value="vendor">Vendor</option>
+                              {{-- <option value="vendor">Vendor</option> --}}
                               <option value="admin">Admin</option>
                             </select>
                           </div>
-                          <button type="submit" class="btn btn-primary">Create</button>
+                          <button type="submit" class="btn btn-primary">Tạo mới</button>
                     </form>
 
                   </div>
