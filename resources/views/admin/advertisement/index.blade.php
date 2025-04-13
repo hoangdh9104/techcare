f22e44f1dacdbb7589b406177c6273ec51aec13a
ducknamya2107 <example@email.com>
2025-04-13 12:01:35 +0700

    fix sidebar adminadmin

--- resources/views/admin/advertisement/index.blade.php
+++ resources/views/admin/advertisement/index.blade.php
@@ -2,12 +2,12 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>Slider</h1>
-            <div class="section-header-breadcrumb">
+            <h1>Thiết lập banner quảng cáo</h1>
+            {{-- <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="#">Components</a></div>
                 <div class="breadcrumb-item">Table</div>
-            </div>
+            </div> --}}
         </div>
 
         <div class="section-body">
