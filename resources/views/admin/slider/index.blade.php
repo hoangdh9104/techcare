a84f6623116570ce9329aae2e909169f91a84f23
ducknamya2107 <example@email.com>
2025-05-03 14:36:10 +0700

    update blogblog

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -3,11 +3,11 @@
     <section class="section">
         <div class="section-header">
             <h1>Slider</h1>
-            <div class="section-header-breadcrumb">
+            {{-- <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Trang chủ</a></div>
                 <div class="breadcrumb-item"><a href="#">Giao diện website</a></div>
                 <div class="breadcrumb-item">Slider</div>
-            </div>
+            </div> --}}
         </div>
 
         <div class="section-body">
@@ -15,7 +15,7 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Simple Table</h4>
+                            <h4>Bảng đơn giản</h4>
                             <div class="card-header-action">
                                 <a href="{{ route('admin.slider.create') }}" class="btn btn-primary"><i
                                         class="fas fa-plus"></i> Tạo mới</a>
